b58ef54aa8eb8a28918bed158ed5baf334d7bcdc
dantoso <xeddantasvfd@gmail.com>
2022-05-10 13:06:11 -0300

    Initial Commit

---
+++ SoundsAndFeelings/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SoundsAndFeelings
+//
+//  Created by Matheus Dantas on 10/05/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SoundsAndFeelings/SoundsAndFeelingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoundsAndFeelingsApp.swift
+//  SoundsAndFeelings
+//
+//  Created by Matheus Dantas on 10/05/22.
+//
+
+import SwiftUI
+
+@main
+struct SoundsAndFeelingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
